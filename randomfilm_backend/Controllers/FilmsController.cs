51fb1f4d21806c589dd8bbfd0d2d977df858fb13
Антон Запариванный <stalkerjva4ka@gmail.com>
2019-09-30 07:56:42 +0300

    1.0

---
+++ randomfilm_backend/Controllers/FilmsController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+using randomfilm_backend.Models;
+
+namespace randomfilm_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilmsController : ControllerBase
+    {
+        private readonly FilmsDBContext _context;
+
+        public FilmsController(FilmsDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Films
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Film>>> GetFilms()
+        {
+            return await _context.Films.ToListAsync();
+        }
+
+        // GET: api/Films/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Film>> GetFilm(int id)
+        {
+            Film film = await _context.Films.FirstOrDefaultAsync((findingFilm) => findingFilm.Id == id);
+
+            if (film == null)
+            {
+                return NotFound();//NotFound();
+            }
+
+            return film;
+        }
+
+        // GET: api/Films/Random
+        [HttpGet("Random")]
+        public async Task<ActionResult<Film>> GetRandomFilm()
+        {
+            return await FilmUtility.GetRandomFilmAsync(_context.Films);
+        }
+
+        // PUT: api/Films/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFilm(int id, Film film)
+        {
+            if (id != film.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(film).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FilmExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        //// POST: api/Films
+        //[HttpPost]
+        //public async Task<ActionResult<Film>> PostFilm([FromBody]Film film)
+        //{
+        //    _context.Films.Add(film);
+        //    try
+        //    {
+        //        await _context.SaveChangesAsync();
+        //    }
+        //    catch (DbUpdateException)
+        //    {
+        //        if (FilmExists(film.Id))
+        //        {
+        //            return Conflict();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
+
+        //    return Ok(film);
+        //    //return CreatedAtAction("GetFilm", new { id = film.Id }, film);
+        //
+
+        // POST: api/Films
+        [HttpPost]
+        public async Task<ActionResult<Film>> PostFilm([FromBody]string stringFilm)
+        {
+            Film film = FilmUtility.GetFilmFromJson(stringFilm);
+            _context.Films.Add(film);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (FilmExists(film.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return Ok(film);
+            //return CreatedAtAction("GetFilm", new { id = film.Id }, film);
+        }
+
+
+        // DELETE: api/Films/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Film>> DeleteFilm(int id)
+        {
+            var film = await _context.Films.FirstOrDefaultAsync((findingFilm) => findingFilm.Id == id);//FindAsync(id);
+            if (film == null)
+            {
+                return NotFound();
+            }
+            _context.Films.Remove(film);
+            await _context.SaveChangesAsync();
+
+            return film;
+        }
+
+        private bool FilmExists(int id)
+        {
+            return _context.Films.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ randomfilm_backend/Models/FilmsDBContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
+
+namespace randomfilm_backend.Models
+{
+    /// <summary>
+    /// Контекст базы данных фильмов "FilmsDB"
+    /// </summary>
+    public partial class FilmsDBContext : DbContext
+    {
+        public FilmsDBContext()
+        { }
+
+        public FilmsDBContext(DbContextOptions<FilmsDBContext> options)
+            : base(options)
+        { }
+
+        public virtual DbSet<Film> Films { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-NMP0LC3\\SQLEXPRESS;Initial Catalog=FilmsDB;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.6-servicing-10079");
+
+            modelBuilder.Entity<Film>(entity =>
+            {
+                entity.Property(e => e.Id)
+                    .HasColumnName("ID")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.Description).HasMaxLength(1000);
+
+                entity.Property(e => e.Director).HasMaxLength(255);
+
+                entity.Property(e => e.Genre).HasMaxLength(255);
+
+                entity.Property(e => e.Title).HasMaxLength(255);
+
+                entity.Property(e => e.UrlImg).HasMaxLength(255);
+
+                entity.Property(e => e.UrlTrailer).HasMaxLength(255);
+
+                entity.Property(e => e.Year).HasColumnType("date");
+            });
+        }
+    }
+}
